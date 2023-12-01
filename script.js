60313c9866db090db38316ad3ed1ba2865f2291a
Grace Do <61570746+grace1501@users.noreply.github.com>
2023-11-30 23:14:44 -0600

    part 3 done

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Part 1: Refactoring Old Code
-
+// Part 2: Expanding Functionality
 // Refactoring code from previous assignment 308.3.1
 
 let csvData = 'ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26';
@@ -28,3 +28,31 @@ for (let i=0; i < dataRows.length ; i++) {
 
 console.log(resultArr);
 
+
+
+// Part 3: Transforming Data
+const inputArr = [
+    ["ID", "Name", "Occupation", "Age"], 
+    ["42", "Bruce", "Knight", "41"], 
+    ["57", "Bob", "Fry Cook", "19"], 
+    ["63", "Blaine", "Quiz Master", "58"], 
+    ["98", "Bill", "Doctor’s Assistant", "26"]
+];
+
+const peopleArray = [];
+
+//starting from the row after header row, loop through each row
+for (let i = 1; i < inputArr.length; i++) {
+    //create a person object for each row in the data
+    let row = inputArr[i];
+    let personObj = {};
+
+    //loop through the array elements of the row and add the key-value pair to personObj
+    for (let k=0; k < row.length; k++) {
+        personObj[inputArr[0][k]] = row[k];
+    }
+    // after finish the row, add personObj to peopleArray
+    peopleArray.push(personObj);
+}
+
+console.log(peopleArray);
